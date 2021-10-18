a91090942361e85f1604d909bc25413efd7bb71e
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-10-18 16:39:34 +0100

    error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -11,9 +15,20 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user,account, profile) {
-      console.log(user);
-      
-      return true
-    }
+      const { email } = user
+
+      try { 
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    },
   }
 })

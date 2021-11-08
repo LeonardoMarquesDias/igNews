64851a22eba301b4431598de4f6f9390f3a82173
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-11-08 17:00:21 +0000

    ActiveLink (error)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,21 @@
-import Link from 'next/link';
-
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="IGNews"/>
         <nav>
-          <Link href="/">
+          <ActiveLink activeClassName={styles.active} href="/">
             <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
-          </Link> 
+          </ActiveLink> 
         </nav>
 
         <SignInButton />

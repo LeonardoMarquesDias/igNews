7781a9d980ded526a1ba5afe1344865781519838
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-11-11 21:58:44 +0000

    PagePosts / ValidatedActiveSubscription

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Prismic from '@prismicio/client'
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 import { GetStaticProps } from 'next';
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           { posts.map(post => (
-              <a key={post.slug} href="">
+            <Link href={post.slug}>
+              <a key={post.slug}>
               <time>{post.updateAt}</time>
               <strong>{post.title}</strong>
               <p>{post.excerpt}</p>
-            </a>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
